5158873ea62278dcf623c36483be08e8b47b7f3a
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-07-04 00:08:27 +0500

    files added

---
+++ index.js
@@ -0,0 +1,16 @@
+function changeColor1() {
+    document.getElementById('color1').style.backgroundColor = "#000000";
+    setTimeout(() => document.getElementById('color1').style.backgroundColor = "", 2000);
+}
+function changeColor2() {
+    document.getElementById('color2').style.backgroundColor = "#216513";
+    setTimeout(() => document.getElementById('color2').style.backgroundColor = "", 2000);
+}
+function changeColor3() {
+    document.getElementById('color3').style.backgroundColor = "#9f9b9b";
+    setTimeout(() => document.getElementById('color3').style.backgroundColor = "", 2000);
+}
+function changeColor4() {
+    document.getElementById('color4').style.backgroundColor = "#a6577d";
+    setTimeout(() => document.getElementById('color4').style.backgroundColor = "", 2000);
+}
